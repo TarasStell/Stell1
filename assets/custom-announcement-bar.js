eb33bfc37afc00688568bef5833095cb234d1843
TarasStell <tdovzenko.stellarsoft@gmail.com>
2022-05-23 20:28:59 +0300

    timer not

--- assets/custom-announcement-bar.js
+++ assets/custom-announcement-bar.js
@@ -1,28 +1,28 @@
-let btn = document.querySelectorAll(".btn-x");
-let block = document.querySelector(".swiper-announcement");
+// let btn = document.querySelectorAll(".btn-x");
+// let block = document.querySelector(".swiper-announcement");
 
-function checkBar() {
-  const clicked = localStorage.getItem("hide-bar");
-  if (!clicked) {
-    block.style.display = "flex";
-    for (let item of btn) {
-      item.addEventListener("click", saveBar);
-    }
-  }
-}
+// function checkBar() {
+//   const clicked = localStorage.getItem("hide-bar");
+//   if (!clicked) {
+//     block.style.display = "block";
+//     for (let item of btn) {
+//       item.addEventListener("click", saveBar);
+//     }
+//   }
+// }
 
-function saveBar() {
-  localStorage.setItem("hide-bar", "true");
-  block.style.display = "none";
-}
+// function saveBar() {
+//   localStorage.setItem("hide-bar", "true");
+//   block.style.display = "none";
+// }
 
-checkBar();
+// checkBar();
 
 const announcementSwiper = new Swiper(".swiper-announcement", {
   slidesPerView: 1,
   speed: 550,
   loop: true,
-  autoplay: {
-    delay: 3000,
-  },
+  // autoplay: {
+  //   delay: 3000,
+  // },
 });
